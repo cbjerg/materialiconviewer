135d264760e55e4ea2c7dec5dad08307ab37d853
Christian Michael Bjerg <cbjerg@gmail.com>
2018-02-09 14:32:36 +0100

    initCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import { ClipboardService } from './services/clipboard.service';
+import { ClipboardDirective } from './directives/clipboard.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClipboardDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [ClipboardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
